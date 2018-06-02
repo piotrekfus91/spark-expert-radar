e0cdd0e79d10eb1447840ac87c8701ab36b39d24
Piotr Fus <piotrek.fus.91@gmail.com>
2018-06-02 13:57:15 +0200

    Add bounding box size indexing to ES

--- src/main/scala/com/github/ser/domain/GeoResult.scala
+++ src/main/scala/com/github/ser/domain/GeoResult.scala
@@ -1,4 +1,8 @@
 package com.github.ser.domain
 
 case class GeoResult(displayName: String, latitude: Double, longitude: Double, importance: Double, boundingBox: BoundingBox)
-case class BoundingBox(westLatitude: Double, eastLatitude: Double, northLongitude: Double, southLongitude: Double)
+
+case class BoundingBox(westLatitude: Double, eastLatitude: Double, northLongitude: Double, southLongitude: Double) {
+  lazy val latSpan = Math.abs(westLatitude - eastLatitude)
+  lazy val lonSpan = Math.abs(northLongitude - southLongitude)
+}
